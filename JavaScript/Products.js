a322b4b67ca9224b17bc4356a727fa99668f009e
Aryss-John <aryssjohn@gmail.com>
2024-07-19 18:03:38 +0800

    First Commit

---
+++ JavaScript/Products.js
@@ -0,0 +1,26 @@
+const products = [
+
+    {
+        "id": 1,
+        "productName": "Hustler's Club",
+        "type": ["Tshirt", "Featured"],
+        "price": 650,
+        "ratings": "4.8",
+        "image1": "https://i.imgur.com/iF6WmEv.jpg",
+        "image2": "https://i.imgur.com/8BD6pe4.jpg",
+        "image3": "https://i.imgur.com/z9kmswI.jpg"
+    },
+
+    {
+        "id": 2,
+        "productName": "Hustle Over Love",
+        "type": ["Tshirt", "Featured"],
+        "price": 650,
+        "ratings": "4.8",
+        "image1": "https://i.imgur.com/9hvZEgC.jpg",
+        "image2": "https://i.imgur.com/GBpuTJW.jpg",
+        "image3": "https://i.imgur.com/TMkIGDI.jpg",
+        "image4": "https://i.imgur.com/y5XTlKV.jpg"
+    },
+  ];
+  
