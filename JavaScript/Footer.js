ee30ecce8426bd4b49afa793bf484bbbf4256497
Aryss-John <aryssjohn@gmail.com>
2024-07-26 10:18:07 +0800

    Product Review Added

--- JavaScript/Footer.js
+++ JavaScript/Footer.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
  <footer class="p-4 bg-zinc-950 md:p-8 lg:p-10">
       <div class="mx-auto max-w-screen-xl text-center">
           <a href="https://www.facebook.com/hustlefriendstreetwear" class="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
-            <img src="Logo/HustleFriends-Logo__NoBG.png" class="h-16 mx-auto sm:h-20 md:h-20 lg:h-20" alt="Hustle Friends Logo" />
+            <img src="/Logo/HustleFriends-Logo__NoBG.png" class="h-16 mx-auto sm:h-20 md:h-20 lg:h-20" alt="Hustle Friends Logo" />
           </a>
           <p class="mb-4 text-zinc-600">For those who hustle nowadaze.</p>
           <ul class="flex flex-wrap justify-center items-center mb-6 text-stone-200">
